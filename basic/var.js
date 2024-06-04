a55f7c1cba726000f4e634b762f4e6e0c8e0c7e5
harsh0482 <140880952+harsh0482@users.noreply.github.com>
2024-06-04 11:06:05 +0000

    congi done

--- basic/var.js
+++ basic/var.js
@@ -1,6 +1,7 @@
 const accountId=1234
+const kanu=1111
 let userName="harsh"
 let password="harh1332"
 userName="harshu"
 password="chandu"
-console.table([accountId,userName,password]);
+console.table([accountId,kanu,userName,password]);
