a6886c319e95ad17a35ca96279a14777026ffd7e
harsh0482 <140880952+harsh0482@users.noreply.github.com>
2024-07-04 09:31:21 +0000

    datatype

--- basic/datatype-conversion.js
+++ basic/datatype-conversion.js
@@ -1,6 +1,15 @@
-let age = 22;
-console.log(typeof(age));
+// let age = 22;
+// console.log(typeof(age));
 
-let agenumber=String(age)
-console.log(typeof(agenumber));
+// let agenumber=String(age)
+// console.log(typeof(agenumber));
 
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+console.log(y);
+
+let x2 = 3n;
+const y2 = ++x2;
+console.log(y2);
+// x2 is 4n; y2 is 4n
