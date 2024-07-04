f6cded65a6b120ff95dae96b97fe0b47ad1b9d9d
harsh0482 <140880952+harsh0482@users.noreply.github.com>
2024-07-04 12:07:57 +0000

    change

--- basic/memory.js
+++ basic/memory.js
@@ -1,2 +1,21 @@
+//Stack  memory
 let uname = "hello"
 console.log(uname);
+let changename=uname
+changename="hyyy"
+console.log(changename)
+
+//Heap memory
+
+const user = {
+    emailId:"xyz@gmail.com",
+    age:33,
+    address:"surat"
+}
+console.log(user);
+
+let user2 = user
+user2.age="44"
+console.log(user2.age)
+console.log(user.age)
+
