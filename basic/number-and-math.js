23bb79d8809354e56aa5830e2d4939bbd06129e3
harsh0482 <140880952+harsh0482@users.noreply.github.com>
2024-07-05 09:01:36 +0000

    commit

---
+++ basic/number-and-math.js
@@ -0,0 +1,8 @@
+const nuber1=new Number(100)
+console.log(nuber1);
+
+const snumber1=new String(nuber1);
+console.log(snumber1);
+
+const number2=100000000
+console.log(number2.toLocaleString('en-IN'));
