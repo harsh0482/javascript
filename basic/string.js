48579557eb2d2be85530a25aa1b1b0bcabc98e85
harsh0482 <140880952+harsh0482@users.noreply.github.com>
2024-07-05 04:18:57 +0000

    commit

---
+++ basic/string.js
@@ -0,0 +1,7 @@
+let userName = 'manubhai kanubhai kkk'
+console.log(userName.length);
+console.log(userName.trim());
+console.log(userName.toUpperCase())
+console.log(userName.toLocaleLowerCase())
+console.log(userName.charAt(2))
+// console.log(userName.indexOf(3))
