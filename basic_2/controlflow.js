95d77541fde05e677f9baaa5093ad3ed2baa5267
harsh0482 <140880952+harsh0482@users.noreply.github.com>
2024-07-12 06:08:56 +0000

    new add

---
+++ basic_2/controlflow.js
@@ -0,0 +1,28 @@
+// const day=2
+// switch (day) {
+//     case 1:
+//         console.log("one (1) ");
+//         break;
+//     case 2:
+//         console.log("two (2)");
+//         break;
+//     case 3:
+//         console.log("three (3)");
+//         break;
+//     case 4:
+//         console.log("four (4)");
+//         break;
+//     default:
+//         console.log("not avalible our number");
+//         break;
+// }
+
+for(let i=0;i<=15;i++){
+    
+    if(i %2==0){
+        console.log(i,"even Number");
+    }
+    else{
+        console.log(i,"odd Number");
+    }
+}
